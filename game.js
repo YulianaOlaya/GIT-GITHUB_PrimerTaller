f5831daf895127a7634f92745924f25d90dd7d4c
Yuliana Olaya <yulianaolayaalbino@gmail.com>
2026-02-12 18:27:50 -0500

    Add attempts counter feature

--- game.js
+++ game.js
@@ -2,10 +2,12 @@ alert("Welcome to the number guessing game ");
 
 let guess = Math.floor(Math.random() * 10) + 1;
 let attempt = 0;
+let totalAttempts = 0;
 
 while (attempt !== guess) {
 
     attempt = parseInt(prompt("Guess the number from 1 to 10"));
+    totalAttempts++;
 
     if (isNaN(attempt) || attempt < 1 || attempt > 10) {
         alert("Enter a valid number between 1 and 10");
@@ -15,7 +17,7 @@ while (attempt !== guess) {
     let difference = Math.abs(guess - attempt);
 
     if (attempt === guess) {
-        alert("guessed");
+        alert("You guessed it in " + totalAttempts + " attempts");
     } 
     else if (difference <= 2) {
         alert("Hot");
